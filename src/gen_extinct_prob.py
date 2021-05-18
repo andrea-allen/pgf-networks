corrected small numerical error in extinction prob and random changes to explore extinct probs for different distributions

src/gen_extinct_prob.py
@@ -13,7 +13,7 @@ def gen_offspring_dist(deg_dist, T):
     l_dist = np.zeros(len(deg_dist))
     for l in range(len(l_dist)):
         for k in range(l,len(l_dist)):
-            l_dist[l] += binom.pmf(l,k,T)*deg_dist[k]
+            l_dist[l] += binom.pmf(l,k,T)*deg_dist[k]*k
     return l_dist/np.sum(l_dist)
 
 # takes a distribution and returns a pgf as a function

src/main_NJR.py
@@ -13,9 +13,9 @@ import numpy as np
 from scipy import stats
 from scipy.optimize import root
 
-import degree_distributions
-import pgf_formalism
-import plotting_util
+import src.degree_distributions as degree_distributions
+import src.pgf_formalism as pgf_formalism
+import src.plotting_util as plotting_util
 
 size = 400
 
@@ -28,7 +28,7 @@ def exp_val_dd(dist):
 power_law_dd = degree_distributions.power_law_degree_distrb(size, mu=150)
 
 # generate time series data for pwr law extinction
-extnct_cake_PL_1, psi_1 = pgf_formalism.compute_extinct_prob_all(power_law_dd, T=0.8, n_gens=20, renorm=False)
+extnct_cake_PL_1, psi_1 = pgf_formalism.compute_extinct_prob_all(power_law_dd, T=0.8, n_gens=11, renorm=True)
 extnct_ts_1 = np.sum(extnct_cake_PL_1, axis=(1,2))
 
 """
@@ -47,28 +47,30 @@ bi_mod_dd = np.zeros(size)
 bi_mod_dd[2] = p_small
 bi_mod_dd[5] = 1 - p_small
 
-extnct_cake_Bin_1, psi_2 = pgf_formalism.compute_extinct_prob_all(binom_dd, T=0.8, n_gens =20, renorm=False)
+extnct_cake_Bin_1, psi_2 = pgf_formalism.compute_extinct_prob_all(binom_dd, T=0.8, n_gens =11, renorm=True)
 extnct_ts_2 = np.sum(extnct_cake_Bin_1, axis=(1,2))
 
-extnct_cake_BiMod_1, psi_2 = pgf_formalism.compute_extinct_prob_all(bi_mod_dd, T=0.8, n_gens =20, renorm=False)
+extnct_cake_BiMod_1, psi_3 = pgf_formalism.compute_extinct_prob_all(bi_mod_dd, T=0.8, n_gens =11, renorm=True)
 extnct_ts_3 = np.sum(extnct_cake_BiMod_1, axis=(1,2))
 
 """
 Plot extinction time series
 """
-fig, ax = plt.subplots()
-ax.scatter(range(20), extnct_ts_1, label = "pwr law")
-ax.scatter(range(20), extnct_ts_2, label = "binom")
-ax.scatter(range(20), extnct_ts_3, label = "bi modal")
-plt.xlabel("generation", fontsize=18)
-plt.ylabel("prob extnct after gen g", fontsize=18)
-plt.legend()
-plt.xticks(range(0,20,2),fontsize=14)
-plt.yticks(fontsize=14)
-plt.tight_layout()
-plt.show()
+# =============================================================================
+# fig, ax = plt.subplots()
+# ax.scatter(range(20), extnct_ts_1, label = "pwr law")
+# ax.scatter(range(20), extnct_ts_2, label = "binom")
+# ax.scatter(range(20), extnct_ts_3, label = "bi modal")
+# plt.xlabel("generation", fontsize=18)
+# plt.ylabel("prob extnct after gen g", fontsize=18)
+# plt.legend()
+# plt.xticks(range(0,20,2),fontsize=14)
+# plt.yticks(fontsize=14)
+# plt.tight_layout()
+# plt.show()
+# =============================================================================
 
 
-extnct_cake_PL_2, psi_new = pgf_formalism.compute_extinct_prob_all(power_law_dd, T=0.8, n_gens=11, renorm=True)
-plotting_util.plot_extinct_prob(extnct_cake_PL_2, x_ax="cumulative infections, s", y_ax="Prob extinction after gen G")
-plotting_util.plot_extinct_prob(extnct_cake_PL_2, x_ax="cumulative infections, s", y_ax="Prob extinction after gen G", hide_zero=True)
+# extnct_cake_PL_2, psi_new = pgf_formalism.compute_extinct_prob_all(power_law_dd, T=0.8, n_gens=11, renorm=True)
+plotting_util.plot_extinct_prob(extnct_cake_BiMod_1, x_ax="cumulative infections, s", y_ax="Prob extinction after gen G")
+#plotting_util.plot_extinct_prob(extnct_cake_PL_2, x_ax="cumulative infections, s", y_ax="Prob extinction after gen G", hide_zero=True)

src/pgf_formalism.py
@@ -6,11 +6,11 @@ from src import gen_extinct_prob
 
 
 def compute_extinct_prob_all(deg_dist, T, n_gens=20, renorm=True):
-    psi = Psi(deg_dist, initProb=1, num_gens=n_gens, max_s=400, max_m=400, initial_T=T)
+    psi = Psi(deg_dist, initProb=1, num_gens=n_gens, max_s=len(deg_dist), max_m=len(deg_dist), initial_T=T)
     for g in range(n_gens):
-        psi[g][:,0] = np.zeros(400)
+        psi[g][:,0] = np.zeros(len(deg_dist))
         for s in range(0,g+1):
-            psi[g][s,:] = np.zeros(400)
+            psi[g][s,:] = np.zeros(len(deg_dist))
         psi[g] = psi[g]/np.sum(psi[g])
     if renorm:
         for g in range(n_gens):

src/plotting_util.py
@@ -134,11 +134,11 @@ def plot_extinct_prob(psi, g_list = (3,5,7,10), sum_ax = 2, x_ax="x axis", y_ax=
     markers = ['ko', 'bs', 'rD', 'mv']
     for g in g_list:
         lbl = "gen" + str(g)
-        plt.plot(s_list, Psi[g][s_list[0]:s_list[-1]+1], markers[g_list.index(g)], label=lbl)
+        plt.plot(s_list, np.log(Psi[g][s_list[0]:s_list[-1]+1]), markers[g_list.index(g)], label=lbl)
     plt.xlabel(x_ax, fontsize=20)
     plt.ylabel(y_ax, fontsize=20)
     plt.xticks(range(0,101,10),fontsize=18)
-    plt.ylim((0,np.max(Psi)+0.05))
+    #plt.ylim((0,np.max(Psi)+0.05))
     plt.yticks(fontsize=18)
     plt.legend()
     plt.tight_layout()